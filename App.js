44d3a2150688a28082ae8275b2d59002a358e861
Nitin Momaya <nitinmomaya5@gmail.com>
2023-01-13 14:45:47 +0530

    Chapter 01- Inception

---
+++ App.js
@@ -0,0 +1,22 @@
+//  Below code shows how to write hello world program with React 
+
+    //{} --> is an object to pass content on specific id {id: "title"}
+    //React will override the content to previous content
+     const heading= React.createElement("h1",{},"Namaste Nitin from React");
+     const root= ReactDOM.createRoot(document.getElementById("root"));
+
+     //passing a react element (heading) to the root
+     root.render(heading);
+
+
+
+
+
+ //Below code show how to write hello world program with JS
+ 
+
+    const headingj=document.createElement("h1");
+    headingj.innerHTML="Namaste Nitin from JavaScript!!";
+
+    const rootj=document.getElementById('jroot');
+    jroot.appendChild(headingj);
